63bc5d5628a7c0444b98d6a672c41cc734ad7c64
swissChili <sasha@fastmail.com>
2018-08-27 12:42:45 -0700

    Returns daily forecast at api request, using provided long/lat

--- src/myst.py
+++ src/myst.py
@@ -1,16 +1,35 @@
 from bottle import get, run, request
+from urllib.parse import urlencode
+from urllib.request import urlopen
+from urllib.error import HTTPError
+import json
 
-api = "https://api.darksky.net/forecast/{key}/{latitude},{longitude}"
+
+ip_api = "http://ipinfo.io/json/127.0.0.1"
+ds_api = "https://api.darksky.net/forecast/{key}/{latitude},{longitude}"
 
 def load_config ( ):
 	with open('config.json', 'r') as cfg:
-		config = json.loads(cfg.read())
+		config = cfg.read()
 		cfg.close()
-	return config
+	print(config)
+	return json.loads(config)
 
-@get('/auto/forecast')
-def get_forecast():
-	ip = request.environ.get('REMOTE_ADDR')
-	print(ip)
+@get('/<lat>/<long>/forecast')
+def get_forecast(lat, long):
+	cfg = load_config()
+	res = urlopen(ds_api.format(key = cfg['keys'][0], latitude = lat, longitude = long)).read()
+	daily = json.loads(res)['daily']['data']
+
+	data = []
+	for i, _ in enumerate(daily):
+		data.append(
+			{
+				"summary": daily[i]['summary'],
+				"high": daily[i]['temperatureHigh'],
+				"low": daily[i]['temperatureLow']
+			}
+		)
+	return json.dumps(data)
 
 run(host='localhost', port=8080)
